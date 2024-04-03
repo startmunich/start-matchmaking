3f1177359e4e9aaf68058b1aa4595ac24a31f3d3
Arian Gohari <a.gohari@startmunich.de>
2024-04-04 00:07:35 +0200

    restructure to make compatible with slack logic

--- services/chat_service.py
+++
@@ -1,6 +0,0 @@
-def write_message(message):
-    print(message)
-
-
-def get_answer(message):
-    return input(f"{message}\n")
---
+++ services/cli_service.py
@@ -0,0 +1,7 @@
+
+def run(mode):
+    if mode == "slack":
+        # slack_service
+        pass
+    elif mode == "console":
+        pass
