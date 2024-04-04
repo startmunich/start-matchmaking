c85407a4c14900a5d8717dcedf46f27614287f02
Yasin Öztürk <yasinoeztuerk02@gmail.com>
2024-04-04 02:26:14 +0200

    first version (not working)

--- services/slack_service.py
+++ services/slack_service.py
@@ -1,3 +1,22 @@
-def start():
-    pass
+import os
 
+from dotenv import load_dotenv
+from slack_bolt import App
+from slack_bolt.adapter.socket_mode import SocketModeHandler
+
+load_dotenv(override=True)
+
+# Install the Slack app and get xoxb- token in advance
+app = App(token=os.environ["SLACK_BOT_TOKEN"])
+
+@app.event("app_mention")
+def event_test(event, say):
+    user = event["user"]
+    channel = event["channel"]
+    text = event["text"]
+    say(f"Hi there, <@{user}>!")
+    print("Received Message\n | channel: " + channel + ", user: " + user + ", text: " + text)
+
+
+if __name__ == "__main__":
+    SocketModeHandler(app, os.environ["SLACK_APP_TOKEN"]).start()
