5beb2cee964e4b568ff4f87b70192b8730d2ae5e
Yasin Öztürk <yasinoeztuerk02@gmail.com>
2024-05-09 00:10:02 +0200

    add working example of RAG functionality for CV content

--- services/db_service.py
+++ services/db_service.py
@@ -1,41 +1,53 @@
 from dotenv import load_dotenv
+from langchain_text_splitters import CharacterTextSplitter
 import requests
 import os
 import tempfile
 
-
-
 from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
-from pypdf import PdfReader
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate
+
 
 # Read .env file
 load_dotenv(override=True)
 
 # Specifies the path of the Chroma vector database, init db
 db_path = "./db"
-db = Chroma(persist_directory=db_path, embedding_function=OpenAIEmbeddings(), collection_name="users")
+embeddings = OpenAIEmbeddings()
+db = Chroma(persist_directory=db_path, embedding_function=embeddings, collection_name="users")
 
+# Specify model
+model = ChatOpenAI(model="gpt-3.5-turbo-0125")
+
+# Define custom prompt
+prompt_template = """Use the following pieces of context to answer the question at the end.
+If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Use three sentences maximum and keep the answer as concise as possible.
+
+{context}
+
+Question: {question}
+
+Answer:"""
+
+# Initialize prompt
+prompt_template = PromptTemplate.from_template(prompt_template)
+
+llm = prompt_template | model
 
 def add_user_by_conversation(_id: str, user_responses: list):
     db.add_texts(ids=[_id], texts=[str(user_responses)], collection_name="users")
 
-# TODO: Read cv from slack conversation using langchain pypdf loader
+
 def add_user_by_cv(_id: str, cv_path: str):
     print("Inside add_user_by_cv")
     print("CV Path:", cv_path)
 
     slack_bot_token = os.environ["SLACK_BOT_TOKEN"]
     auth_header = {'Authorization': f'Bearer {slack_bot_token}'}
-
-    # Download PDF file locally to specific location
-    # pdf_filename = cv_path.split('/')[-1]
-    # pdf_path = f'/Users/yasinozturk/Downloads/matchmaking testing/{pdf_filename}'
-    # response = requests.get(cv_path, headers=auth_header)
-    # print(response)
-    # with open(pdf_path, 'wb') as f:
-    #     f.write(response.content)
 
     # Create a temporary directory
     with tempfile.TemporaryDirectory() as temp_dir:
@@ -60,15 +72,29 @@ def add_user_by_cv(_id: str, cv_path: str):
             loader = PyPDFLoader(pdf_path)
             pages = loader.load_and_split()
             print(pages)
+            # Split extracted text into chunks
+            text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+            docs = text_splitter.split_documents(pages)
+            # Extract page_content
+            # page_contents = [page.page_content for page in pages]  
+
         else:
             print("Failed to download the file.")
 
+    # Add split text from CV to the Chroma database 
+    db.add_documents(documents=docs, collection_name="users")
 
-    # Extract text from the downloaded PDF using vanilla PyPDF
-    # reader = PdfReader(pdf_path)
-    # number_of_pages = len(reader.pages)
-    # page = reader.pages[0]
-    # text = page.extract_text()
-    # print(text)
+    question = input("Ask a question about the CV: ")
+
+    # Perform a similarity search based on the user's question
+    context = db.similarity_search(question)
+
+    # Get answer based on the provided context and the user's question
+    response = llm.invoke({"question": question, "context": context})
+    print("Reponse: ", response.content)
+
+
+
+
 
 
