b6d7dde0ec4550663d33bb6bb8243d6e1daa0ff9
Khadim Fall <khadim@MacBook-Pro-von-Khadim.local>
2024-09-28 18:21:57 +0200

    :S

---
+++ services/utils.py
@@ -0,0 +1,13 @@
+import requests
+import os
+import tempfile
+
+async def download_temp(_url: str, _auth_header: dict[str, str]) -> str:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        pdf_filename = _url.split('/')[-1]
+        pdf_path = os.path.join(temp_dir, pdf_filename)
+        response = requests.get(_url, headers=_auth_header)
+
+        with open(pdf_path, 'wb') as f:
+            f.write(response.content)
+        return pdf_path
